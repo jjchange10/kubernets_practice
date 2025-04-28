71b6de8d9b427cdd3371cc9590c8d9c640846c90
jjchange10 <jj.change10@icloud.com>
2025-04-28 20:25:37 +0900

    add Dockerfile

---
+++ terraform/iam.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "github_actions_role" {
+  name = "GitHubActionsECRPushRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:jjchange10/kubernets_practice:*",
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_ecr_policy" {
+  name = "GitHubActionsECRPolicy"
+  role = aws_iam_role.github_actions_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
